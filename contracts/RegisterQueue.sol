894780736786eddb5a82f590aa043dcb724b7ce5
StoyanD <s.dimitrov19@gmail.com>
2017-11-02 13:57:20 -0700

    adding shell of contract

--- contracts/RegisterQueue.sol
+++ contracts/RegisterQueue.sol
@@ -2,5 +2,26 @@ pragma solidity ^0.4.17;
 import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 
 contract RegisterQueue is Ownable {
+    int public queueFront = 0;
 
+    //Don't send money to this contract
+    function(){
+      revert();
+    }
+
+    //Register user into the queue at the end of the line, return the
+    //position of the user once inserted
+    function registerUser(address referrerAddress) returns (int){
+      return 1;
+    }
+
+    //Move any user in the queue to a point where they are allowed
+    //access to the app utilizing this contract
+    function skipLine(address userAddress) onlyOwner returns (int){
+      return 1;
+    }
+
+    function getQueueFront() view returns (int){
+      return queueFront;
+    }
 }
