0f940470f671bb12e5576d9c527e4e316f475a6a
StoyanD <s.dimitrov19@gmail.com>
2017-11-02 13:16:08 -0700

    init commit of contract and project

---
+++ contracts/RegisterQueue.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.4.17;
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+
+contract RegisterQueue is Ownable {
+
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+var RegisterQueue = artifacts.require("./RegisterQueue.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(RegisterQueue);
+};
