534d197720f7e119d3fe7225b13ba34a5f81865e
StoyanD <s.dimitrov19@gmail.com>
2017-11-03 10:53:05 -0700

    adding test skelleton

---
+++ test/RegisterQueueTests.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.13;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+
+contract RegisterQueueTests {
+  
+  function testRegisteringUsers(){
+
+  }
+
+  function testSkipLine(){
+
+  }
+
+  function testUserApproved(){
+
+  }
+
+  function testSendEtherFail(){
+
+  }
+}
